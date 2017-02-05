d9ba76f39b6d512ccd459a3b504bc1e57ac3aa72
gaurav <gaurav@ggvaidya.com>
2017-02-05 03:15:22 -0500

    Added support for Heroku port, it looks like it can install this correctly so let's not push the JAR.

--- src/main/java/org/phyloref/phyloderef/PhyloDeRef.java
+++ src/main/java/org/phyloref/phyloderef/PhyloDeRef.java
@@ -253,7 +253,17 @@ class OWLFile {
 }
 
 public class PhyloDeRef {
+	private static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+	
     public static void main(String[] args) {
+		port(getHerokuAssignedPort());
+		
         OWLFile crowl_et_al;
         try {
             crowl_et_al = new OWLFile(
